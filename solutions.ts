cdb63dca7d2529d9ff387decc35fa32d3af5aa6f
areful75 <mrarefulomi@gmail.com>
2026-05-27 22:36:55 +0600

    added problem5 solution

--- solutions.ts
+++ solutions.ts
@@ -27,5 +27,20 @@ function checkType(data: StringOrNumber): string {
 
 function getProperty<X, Y extends keyof X>(obj: X, key: Y) {
     return obj[key];
-} 
+}
 
+interface Book {
+    title: string;
+    author: string;
+    publishedYear: number;
+}
+function toggleReadStatus(bookInformation: Book) {
+    return {
+        ...bookInformation,
+        isRead: true,
+
+    };
+
+} 
+const myBook = { title: "TypeScript Guide", author: "Jane Doe", publishedYear: 2024 };
+console.log(toggleReadStatus(myBook));
