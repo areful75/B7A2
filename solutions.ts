d871d4f892262315280b7625019fb2e65028d068
areful75 <mrarefulomi@gmail.com>
2026-05-27 22:16:19 +0600

    added problem3 solution

--- solutions.ts
+++ solutions.ts
@@ -1,15 +1,25 @@
 function filterEvenNumbers(values: number[]): number[] {
     const evenNumberArray = values.filter((item) => item % 2 === 0);
-  
     return evenNumberArray;
-  }
+}
 
-  function reverseString(input: string): string {
-
-    const reverseString=input.split("").reverse().join("");
-
+function reverseString(input: string): string {
+    const reverseString = input.split("").reverse().join("");
     return reverseString;
 
-  }
+}
 
-  
+type StringOrNumber = string | number;
+function checkType(data: StringOrNumber): string {
+    if (typeof data === "string") {
+
+        return "String";
+
+    }
+    else {
+
+        return "Number";
+
+    }
+
+}
